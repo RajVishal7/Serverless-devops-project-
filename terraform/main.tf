82234c388c3c06ad72c927cd93c6ed47a98d48c8
RajVishal7 <rajvishal2775@gmail.com>
2026-03-28 12:51:34 +0530

    added lambda logging permission

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_lambda_function" "lambda" {
-  function_name = "devops-pro-lambda"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  filename      = "../app/function.zip"
-}
-
+# IAM Role for Lambda
 resource "aws_iam_role" "lambda_role" {
   name = "lambda-role"
 
@@ -23,4 +16,26 @@ resource "aws_iam_role" "lambda_role" {
       }
     }]
   })
+}
+
+#  Attach CloudWatch Logs Permission (IMPORTANT FIX)
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Lambda Function
+resource "aws_lambda_function" "lambda" {
+  function_name = "devops-pro-lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+  filename      = "../app/function.zip"
+
+  # Best Practice (detect changes in zip)
+  source_code_hash = filebase64sha256("../app/function.zip")
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs
+  ]
 }
