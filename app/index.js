e219844b0aa4fb2cff6bcec1e885e8ba9f866087
RajVishal7 <rajvishal2775@gmail.com>
2026-03-28 12:37:59 +0530

    test alarm

--- app/index.js
+++ app/index.js
@@ -1,6 +1,4 @@
+
 exports.handler = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify("Hello from PRO DevOps great Project 🚀"),
-    };
+    throw new Error("Testing CloudWatch Alarm 🚨");
 };
